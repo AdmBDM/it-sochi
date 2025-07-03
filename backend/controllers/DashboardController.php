936bca4a3b1ec122c236a90ee3a1b22baf499f40
Дмитрий Белик <jim7nvkz@gmail.com>
2025-07-03 17:24:17 +0300

    Авторизация при входе в дашборд

--- backend/controllers/DashboardController.php
+++ backend/controllers/DashboardController.php
@@ -3,9 +3,29 @@
 namespace backend\controllers;
 
 use common\controllers\SochiMainController;
+use yii\filters\AccessControl;
 
 class DashboardController extends SochiMainController
 {
+    /**
+     * @return array[]
+     */
+    public function behaviors(): array
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'only' => ['index', /* другие действия */],
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],  // только авторизованные пользователи
+                    ],
+                ],
+            ],
+        ];
+    }
+
     /**
      * @return string
      */
